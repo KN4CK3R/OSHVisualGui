c4e251ff0f220c5e66e6d6670750ba423a3fdfb1
KN4CK3R <KN4CK3R@users.noreply.github.com>
2015-08-22 15:07:19 +0000

    -fixed warnings

--- FastColoredTextBox/Place.cs
+++ FastColoredTextBox/Place.cs
@@ -1,78 +1,109 @@
 ﻿
 namespace FastColoredTextBoxNS
 {
-    /// <summary>
-    /// Line index and char index
-    /// </summary>
-    public struct Place
-    {
-        public int iChar;
-        public int iLine;
+	/// <summary>
+	/// Line index and char index
+	/// </summary>
+	public struct Place
+	{
+		public int iChar;
+		public int iLine;
 
-        public Place(int iChar, int iLine)
-        {
-            this.iChar = iChar;
-            this.iLine = iLine;
-        }
+		public Place(int iChar, int iLine)
+		{
+			this.iChar = iChar;
+			this.iLine = iLine;
+		}
 
-        public void Offset(int dx, int dy)
-        {
-            iChar += dx;
-            iLine += dy;
-        }
+		public void Offset(int dx, int dy)
+		{
+			iChar += dx;
+			iLine += dy;
+		}
 
-        public static bool operator !=(Place p1, Place p2)
-        {
-            return !p1.Equals(p2);
-        }
+		public override bool Equals(object obj)
+		{
+			if (obj is Place)
+			{
+				var other = (Place)obj;
 
-        public static bool operator ==(Place p1, Place p2)
-        {
-            return p1.Equals(p2);
-        }
+				return iChar == other.iChar && iLine == other.iLine;
+			}
 
-        public static bool operator <(Place p1, Place p2)
-        {
-            if (p1.iLine < p2.iLine) return true;
-            if (p1.iLine > p2.iLine) return false;
-            if (p1.iChar < p2.iChar) return true;
-            return false;
-        }
+			return base.Equals(obj);
+		}
 
-        public static bool operator <=(Place p1, Place p2)
-        {
-            if (p1.Equals(p2)) return true;
-            if (p1.iLine < p2.iLine) return true;
-            if (p1.iLine > p2.iLine) return false;
-            if (p1.iChar < p2.iChar) return true;
-            return false;
-        }
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
 
-        public static bool operator >(Place p1, Place p2)
-        {
-            if (p1.iLine > p2.iLine) return true;
-            if (p1.iLine < p2.iLine) return false;
-            if (p1.iChar > p2.iChar) return true;
-            return false;
-        }
+		public static bool operator ==(Place p1, Place p2)
+		{
+			return p1.Equals(p2);
+		}
 
-        public static bool operator >=(Place p1, Place p2)
-        {
-            if (p1.Equals(p2)) return true;
-            if (p1.iLine > p2.iLine) return true;
-            if (p1.iLine < p2.iLine) return false;
-            if (p1.iChar > p2.iChar) return true;
-            return false;
-        }
+		public static bool operator !=(Place p1, Place p2)
+		{
+			return !p1.Equals(p2);
+		}
 
-        public static Place Empty
-        {
-            get { return new Place(); }
-        }
+		public static bool operator <(Place p1, Place p2)
+		{
+			if (p1.iLine < p2.iLine)
+				return true;
+			if (p1.iLine > p2.iLine)
+				return false;
+			if (p1.iChar < p2.iChar)
+				return true;
+			return false;
+		}
 
-        public override string ToString()
-        {
-            return "(" + iChar + "," + iLine + ")";
-        }
-    }
+		public static bool operator <=(Place p1, Place p2)
+		{
+			if (p1.Equals(p2))
+				return true;
+			if (p1.iLine < p2.iLine)
+				return true;
+			if (p1.iLine > p2.iLine)
+				return false;
+			if (p1.iChar < p2.iChar)
+				return true;
+			return false;
+		}
+
+		public static bool operator >(Place p1, Place p2)
+		{
+			if (p1.iLine > p2.iLine)
+				return true;
+			if (p1.iLine < p2.iLine)
+				return false;
+			if (p1.iChar > p2.iChar)
+				return true;
+			return false;
+		}
+
+		public static bool operator >=(Place p1, Place p2)
+		{
+			if (p1.Equals(p2))
+				return true;
+			if (p1.iLine > p2.iLine)
+				return true;
+			if (p1.iLine < p2.iLine)
+				return false;
+			if (p1.iChar > p2.iChar)
+				return true;
+			return false;
+		}
+
+		public static Place Empty
+		{
+			get { return new Place(); }
+		}
+
+		public override string ToString()
+		{
+			return "(" + iChar + "," + iLine + ")";
+		}
+	}
 }
