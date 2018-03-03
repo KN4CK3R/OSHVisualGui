fb2745675ff1846a19db9a50b53914d283842f6d
igromanru <igromanru@yahoo.de>
2018-03-03 11:47:52 +0100

    formatting

--- GuiControls/TabPage.cs
+++ GuiControls/TabPage.cs
@@ -6,162 +6,162 @@ using System.Xml.Linq;
 
 namespace OSHVisualGui.GuiControls
 {
-    [TypeConverter(typeof(ExpandableObjectConverter))]
-    public class TabPage : Panel
-    {
-        #region Properties
+	[TypeConverter(typeof(ExpandableObjectConverter))]
+	public class TabPage : Panel
+	{
+		#region Properties
 
-        internal override string DefaultName => "tabPage";
+		internal override string DefaultName => "tabPage";
 
-        internal TabControl.TabControlButton button;
+		internal TabControl.TabControlButton button;
 
-        private readonly Panel containerPanel;
+		private readonly Panel containerPanel;
 
-        protected string text;
-        protected string DefaultText;
-        public string Text
-        {
-            get => text;
-            set
-            {
-                text = value ?? string.Empty;
-                button?.CalculateSize();
-            }
-        }
-        internal override List<Control> Controls => containerPanel.Controls;
+		protected string text;
+		protected string DefaultText;
+		public string Text
+		{
+			get => text;
+			set
+			{
+				text = value ?? string.Empty;
+				button?.CalculateSize();
+			}
+		}
+		internal override List<Control> Controls => containerPanel.Controls;
 
-        [Browsable(false)]
-        public override bool AutoSize
-        {
-            get => base.AutoSize;
-            set => base.AutoSize = value;
-        }
-        [Browsable(false)]
-        public override bool Enabled
-        {
-            get => base.Enabled;
-            set => base.Enabled = value;
-        }
-        [Browsable(false)]
-        public override bool Visible
-        {
-            get => base.Visible;
-            set => base.Visible = value;
-        }
-        [Browsable(false)]
-        public override Point Location
-        {
-            get => base.Location;
-            set => base.Location = value;
-        }
-        [Browsable(false)]
-        public override Font Font
-        {
-            get => base.Font;
-            set => base.Font = value;
-        }
-        [Browsable(false)]
-        public override Size Size
-        {
-            get => base.Size;
-            set
-            {
-                base.Size = value;
-                // For some reason Panel() is called before TabPage() ¯\_(ツ)_/¯
-                if (containerPanel != null)
-                {
-                    containerPanel.Size = value.Substract(new Size(4, 4));
-                }
-            }
-        }
+		[Browsable(false)]
+		public override bool AutoSize
+		{
+			get => base.AutoSize;
+			set => base.AutoSize = value;
+		}
+		[Browsable(false)]
+		public override bool Enabled
+		{
+			get => base.Enabled;
+			set => base.Enabled = value;
+		}
+		[Browsable(false)]
+		public override bool Visible
+		{
+			get => base.Visible;
+			set => base.Visible = value;
+		}
+		[Browsable(false)]
+		public override Point Location
+		{
+			get => base.Location;
+			set => base.Location = value;
+		}
+		[Browsable(false)]
+		public override Font Font
+		{
+			get => base.Font;
+			set => base.Font = value;
+		}
+		[Browsable(false)]
+		public override Size Size
+		{
+			get => base.Size;
+			set
+			{
+				base.Size = value;
+				// For some reason Panel() is called before TabPage() ¯\_(ツ)_/¯
+				if (containerPanel != null)
+				{
+					containerPanel.Size = value.Substract(new Size(4, 4));
+				}
+			}
+		}
 
-        #endregion
+		#endregion
 
-        public TabPage()
-        {
-            Type = ControlType.TabPage;
+		public TabPage()
+		{
+			Type = ControlType.TabPage;
 
-            Mode = DragMode.None;
+			Mode = DragMode.None;
 
-            button = null;
+			button = null;
 
-            containerPanel = new Panel
-            {
-                Location = new Point(2, 2),
-                IsSubControl = true,
-                Parent = this
-            };
-            AddSubControl(containerPanel);
+			containerPanel = new Panel
+			{
+				Location = new Point(2, 2),
+				IsSubControl = true,
+				Parent = this
+			};
+			AddSubControl(containerPanel);
 
-            Text = DefaultText = string.Empty;
+			Text = DefaultText = string.Empty;
 
-            ForeColor = DefaultForeColor = Color.White;
-            BackColor = DefaultBackColor = Color.FromArgb(unchecked((int)0xFF474747));
+			ForeColor = DefaultForeColor = Color.White;
+			BackColor = DefaultBackColor = Color.FromArgb(unchecked((int)0xFF474747));
 
-            //isSubControl = true;
-        }
+			//isSubControl = true;
+		}
 
-        public override IEnumerable<KeyValuePair<string, ChangedProperty>> GetChangedProperties()
-        {
-            yield return new KeyValuePair<string, ChangedProperty>("name", new ChangedProperty(Name));
-            if (ForeColor != DefaultForeColor)
-                yield return new KeyValuePair<string, ChangedProperty>("forecolor", new ChangedProperty(ForeColor));
-            if (BackColor != DefaultBackColor)
-                yield return new KeyValuePair<string, ChangedProperty>("backcolor", new ChangedProperty(BackColor));
-            if (Text != DefaultText)
-                yield return new KeyValuePair<string, ChangedProperty>("text", new ChangedProperty(Text));
-        }
+		public override IEnumerable<KeyValuePair<string, ChangedProperty>> GetChangedProperties()
+		{
+			yield return new KeyValuePair<string, ChangedProperty>("name", new ChangedProperty(Name));
+			if (ForeColor != DefaultForeColor)
+				yield return new KeyValuePair<string, ChangedProperty>("forecolor", new ChangedProperty(ForeColor));
+			if (BackColor != DefaultBackColor)
+				yield return new KeyValuePair<string, ChangedProperty>("backcolor", new ChangedProperty(BackColor));
+			if (Text != DefaultText)
+				yield return new KeyValuePair<string, ChangedProperty>("text", new ChangedProperty(Text));
+		}
 
-        public override void AddControl(Control control)
-        {
-            containerPanel.AddControl(control);
-        }
+		public override void AddControl(Control control)
+		{
+			containerPanel.AddControl(control);
+		}
 
-        public override void RemoveControl(Control control)
-        {
-            containerPanel.RemoveControl(control);
-        }
+		public override void RemoveControl(Control control)
+		{
+			containerPanel.RemoveControl(control);
+		}
 
-        public override void Render(Graphics graphics)
-        {
-            if (BackColor.A > 0)
-            {
-                Brush brush = new SolidBrush(BackColor.Add(Color.FromArgb(0, 32, 32, 32)));
-                graphics.FillRectangle(brush, AbsoluteLocation.X, AbsoluteLocation.Y, Size.Width, Size.Height);
-                var rect = new Rectangle(AbsoluteLocation.X + 1, AbsoluteLocation.Y + 1, Size.Width - 2, Size.Height - 2);
-                brush = new LinearGradientBrush(rect, BackColor, BackColor.Substract(Color.FromArgb(0, 20, 20, 20)), LinearGradientMode.Vertical);
-                graphics.FillRectangle(brush, rect);
-            }
+		public override void Render(Graphics graphics)
+		{
+			if (BackColor.A > 0)
+			{
+				Brush brush = new SolidBrush(BackColor.Add(Color.FromArgb(0, 32, 32, 32)));
+				graphics.FillRectangle(brush, AbsoluteLocation.X, AbsoluteLocation.Y, Size.Width, Size.Height);
+				var rect = new Rectangle(AbsoluteLocation.X + 1, AbsoluteLocation.Y + 1, Size.Width - 2, Size.Height - 2);
+				brush = new LinearGradientBrush(rect, BackColor, BackColor.Substract(Color.FromArgb(0, 20, 20, 20)), LinearGradientMode.Vertical);
+				graphics.FillRectangle(brush, rect);
+			}
 
-            containerPanel.Render(graphics);
-        }
+			containerPanel.Render(graphics);
+		}
 
-        public override Control Copy()
-        {
-            var copy = new TabPage();
-            CopyTo(copy);
-            return copy;
-        }
+		public override Control Copy()
+		{
+			var copy = new TabPage();
+			CopyTo(copy);
+			return copy;
+		}
 
-        protected override void CopyTo(Control copy)
-        {
-            base.CopyTo(copy);
+		protected override void CopyTo(Control copy)
+		{
+			base.CopyTo(copy);
 
-            var tabPage = copy as TabPage;
-            tabPage.Text = text;
-        }
+			var tabPage = copy as TabPage;
+			tabPage.Text = text;
+		}
 
-        public override string ToString()
-        {
-            return Name + " - TabPage";
-        }
+		public override string ToString()
+		{
+			return Name + " - TabPage";
+		}
 
-        public override void ReadPropertiesFromXml(XElement element)
-        {
-            base.ReadPropertiesFromXml(element);
+		public override void ReadPropertiesFromXml(XElement element)
+		{
+			base.ReadPropertiesFromXml(element);
 
-            if (element.HasAttribute("text"))
-                Text = Text.FromXMLString(element.Attribute("text").Value.Trim());
-        }
-    }
+			if (element.HasAttribute("text"))
+				Text = Text.FromXMLString(element.Attribute("text").Value.Trim());
+		}
+	}
 }
